9413c5a6e2cbe10f842985593d49b235900d9e11
tpals11149@gmail.com <semin-pk>
2024-01-31 12:08:08 +0900

    'cors'

--- ToDoproject/settings.py
+++ ToDoproject/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todoapp',
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = ['hhtp://127.0.0.1:3000', 'http://localhost:3000']
+CORS_ALLOW_CREDENTIALS =True
 
 ROOT_URLCONF = 'ToDoproject.urls'
 
